58e405768973b859fde86632768569be6bd2d572
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-07-16 23:49:25 +0530

    new code

---
+++ game.js
@@ -0,0 +1,8 @@
+const wordList = [
+  { word: "apple", hint: "A common fruit" },
+  { word: "python", hint: "A programming language and a snake" },
+  { word: "banana", hint: "Yellow fruit loved by monkeys" },
+  { word: "react", hint: "A JavaScript library" },
+  { word: "school", hint: "Place to learn" },
+  { word: "winter", hint: "Cold season" }
+];
